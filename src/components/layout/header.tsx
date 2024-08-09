ab0ff873b74b2ea66993b2e3500ee5efbaeb5732
Liz Chung <lizzhongx@gmail.com>
2024-08-09 15:26:56 +0900

    no message

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,6 @@
 
 import styled from 'styled-components';
 import { useRouter } from 'next/navigation';
-
 import HamburgerIcon from '@/components/svg/hamburger';
 import BackIcon from '@/components/svg/arrowLeft';
 import HomeIcon from '@/components/svg/home';
