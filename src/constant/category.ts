11cbc503760982b1a63ee94fa8e9eec21deccf1e
Chaewon <cw06155@gmail.com>
2024-08-11 07:32:29 +0900

    💄 Import, update category svg styles
    Import, update category svg styles

--- src/constant/category.ts
+++ src/constant/category.ts
@@ -1,15 +1,16 @@
 export const RESTAURANT_CATEGORIES = {
-  '족발·보쌈': '/fee.svg',
-  '찜·탕·찌개': '/fee.svg',
-  '돈까스·회·일식': '/fee.svg',
-  피자: '/fee.svg',
-  '고기·구이': '/fee.svg',
-  치킨: '/fee.svg',
-  중식: '/fee.svg',
-  아시안: '/fee.svg',
-  '카페·디저트': '/fee.svg',
-  패스트푸드: '/fee.svg',
-  분식: '/fee.svg',
-  기타: '/fee.svg',
+  '족발·보쌈': '/jokbal-bossam.svg',
+  '찜·탕·찌개': '/stew-soup-stew.svg',
+  '돈까스·회·일식': '/tonkatsu-sashimi-japanese.svg',
+  피자: '/pizza.svg',
+  '고기·구이': '/meat-grill.svg',
+  치킨: '/chicken.svg',
+  중식: '/chinese.svg',
+  아시안: '/asian.svg',
+  '카페·디저트': '/cafe-dessert.svg',
+  패스트푸드: '/fastfood.svg',
+  분식: '/bunsik.svg',
+  기타: '/etc.svg',
 };
+
 export type RestaurantCategory = keyof typeof RESTAURANT_CATEGORIES;
