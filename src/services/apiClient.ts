bb862bfb62a2617b2446c9d71837df85605d1f0d
Chaen <cw06155@gmail.com>
2024-08-23 17:31:16 +0900

    Revert "Updated with backend URL"

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,15 +1,9 @@
 import axios from 'axios';
 import qs from 'qs';
 
-const baseURL = process.env.NEXT_PUBLIC_SERVER_API_URL;
-
 // 기본 API 클라이언트 생성
 // const response = await apiClient.get( RESTAURANT_API_URL.replace('{storeId}', storeId.toString()),);
 export const apiClient = axios.create({
-  baseURL,
   paramsSerializer: (params) =>
     qs.stringify(params, { arrayFormat: 'brackets' }),
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-  },
 });
