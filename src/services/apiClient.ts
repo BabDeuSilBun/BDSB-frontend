e489e8f181af8b84d3c9617fd5893c2e4d5ad1c7
Chaewon <cw06155@gmail.com>
2024-08-23 18:45:22 +0900

    env, api Client

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,9 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import qs from 'qs';
 
-// 기본 API 클라이언트 생성
-// const response = await apiClient.get( RESTAURANT_API_URL.replace('{storeId}', storeId.toString()),);
-export const apiClient = axios.create({
-  paramsSerializer: (params) =>
-    qs.stringify(params, { arrayFormat: 'brackets' }),
+interface CreateApiClientParams {
+  withCredentials?: boolean; // withCredentials 옵션을 매개변수로 받음
+}
+
+export const createApiClient = ({
+  withCredentials = false, // 기본값은 false
+}: CreateApiClientParams = {}): AxiosInstance => {
+  const baseURL =
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_SERVER_API_URL
+      : process.env.NEXT_PUBLIC_BACKEND_URL;
+
+  return axios.create({
+    baseURL,
+    withCredentials,
+    paramsSerializer: (params) =>
+      qs.stringify(params, { arrayFormat: 'brackets' }),
+  });
+};
+
+// 기본 사용 예시
+export const apiClient = createApiClient();
+
+// 인증 정보가 필요한 요청에 사용하는 예시
+export const apiClientWithCredentials = createApiClient({
+  withCredentials: true,
 });
