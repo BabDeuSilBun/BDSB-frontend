d4bf723b81310b61a5384328c6748803955467ea
Chaewon <cw06155@gmail.com>
2024-08-28 05:27:41 +0900

    리팩토링

--- src/app/myPage/evaluate/page.tsx
+++ src/app/myPage/evaluate/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
-import Image from 'next/image';
-
-import { Divider, Flex } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import styled from 'styled-components';
 
+import EvaluationSection from './evaluationSection';
+
 import Header from '@/components/layout/header';
-import GroupIcon from '@/components/svg/group';
 import { getMyEvaluates } from '@/services/myDataService';
 
 const Container = styled.div`
@@ -18,17 +16,6 @@ const Container = styled.div`
 const EvaluatesContainer = styled.section`
   margin-top: 70px;
   background: white;
-`;
-
-const Title = styled.h2`
-  font-weight: var(--font-semi-bold);
-  font-size: var(--font-size-xl);
-`;
-const HeadCount = styled.div`
-  display: flex;
-  gap: 8px;
-  justify-content: right;
-  font-size: var(--font-size-sm);
 `;
 
 const Info = styled.div`
@@ -44,63 +31,24 @@ const EvaluatesDetail = () => {
     queryFn: getMyEvaluates,
   });
 
-  if (isLoading) return <p>로딩 중...</p>;
-  if (isError || !data) return <p>에러가 발생했습니다.</p>;
-
   return (
     <Container>
       <Header text="평가 상세" buttonLeft="back" />
       <EvaluatesContainer>
-        <div>
-          <Flex align="center" gap="1" p="1rem">
-            <Image
-              src={'/smiley.svg'}
-              alt="웃는 이미지"
-              width={32}
-              height={32}
-            />
-            <Title>받은 긍정 평가 배지</Title>
-          </Flex>
-          <ul>
-            {data.positiveEvaluate.map((item, index) => (
-              <li key={index}>
-                <Flex justify="space-between" p="1rem">
-                  <p>{item.content}</p>
-                  <HeadCount>
-                    <GroupIcon color="var(--gray300)" width={18} height={18} />
-                    <span>{item.count}</span>
-                  </HeadCount>
-                </Flex>
-                {index < data.positiveEvaluate.length - 1 && <Divider />}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <Flex align="center" gap="1" p="1rem" mt="2rem">
-            <Image
-              src={'/sadly.svg'}
-              alt="슬픈 이미지"
-              width={32}
-              height={32}
-            />
-            <Title>받은 부정 평가 배지</Title>
-          </Flex>
-          <ul>
-            {data.negativeEvaluate.map((item, index) => (
-              <li key={index}>
-                <Flex justify="space-between" p="1rem">
-                  <p>{item.content}</p>
-                  <HeadCount>
-                    <GroupIcon color="var(--gray300)" width={18} height={18} />
-                    <span>{item.count}</span>
-                  </HeadCount>
-                </Flex>
-                {index < data.negativeEvaluate.length - 1 && <Divider />}
-              </li>
-            ))}
-          </ul>
-        </div>
+        <EvaluationSection
+          title="받은 긍정 평가 배지"
+          imageSrc="/smiley.svg"
+          evaluations={data?.positiveEvaluate || []}
+          isLoading={isLoading}
+          isError={isError}
+        />
+        <EvaluationSection
+          title="받은 부정 평가 배지"
+          imageSrc="/sadly.svg"
+          evaluations={data?.negativeEvaluate || []}
+          isLoading={isLoading}
+          isError={isError}
+        />
       </EvaluatesContainer>
       <Info>
         <p>· 받은 부정 평가 배지는 나에게만 보입니다.</p>
