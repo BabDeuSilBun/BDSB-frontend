4134bb7ada8119de3da1974095567077045fe9a2
Liz Chung <lizzhongx@gmail.com>
2024-09-04 10:54:57 +0900

    no message

--- src/mocks/handlers/authHandlers.js
+++ src/mocks/handlers/authHandlers.js
@@ -14,8 +14,8 @@ import {
   SCHOOL_LIST_API_URL,
 } from '@/services/auth/signUpService';
 
-const JWT_SECRET = 'your_secret_key'; // JWT 서명에 사용할 비밀 키
-const JWT_EXPIRATION = '1h'; // JWT의 만료 시간 (1시간으로 설정)
+// const JWT_SECRET = 'your_secret_key'; // JWT 서명에 사용할 비밀 키
+// const JWT_EXPIRATION = '1h'; // JWT의 만료 시간 (1시간으로 설정)
 
 export const authHandlers = [
   http.post('/api/users/signin', async ({ request }) => {
